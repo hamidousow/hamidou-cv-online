9c1f908aa6840c96e76cbd6eb19cf69e81e95838
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-08-10 16:26:06 +0200

    setting dark mode

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const btnBurger = document.querySelector('#btn-burger');
 const btnCloseBtnBurger = document.querySelector('#btn-close-menu')
 const asideMenu = document.querySelector('#aside-menu');
-const switchTheme = document.querySelector('#change-mode');
 
-btnBurger.addEventListener('click', () => {
-    /*console.log('yes');*/   
+// for switch theme
+const btnSwitchTheme = document.querySelector('#btn-change-mode');
+const body = document.querySelector('#body');
+const containerSite = document.querySelector('.container-site');
+
+
+
+// for the burger menu 
+btnBurger.addEventListener('click', () => {     
     asideMenu.classList.remove('hide');
-    asideMenu.classList.add('show');
-    /*asideMenu.style.visibility = 'show';*/
+    asideMenu.classList.add('show');  
     asideMenu.style.width = '70%';
     btnBurger.classList.add('hide');
-    btnCloseBtnBurger.classList.add('show');
+    btnCloseBtnBurger.classList.add('show');    
 });
 
 /*btnCloseBtnBurger.addEventListener('click', () => {   
@@ -19,8 +24,21 @@ btnBurger.addEventListener('click', () => {
     asideMenu.classList.add('hide');
 });*/
 
-switchTheme.addEventListener('click', () => {
-    console.log('yes');
-})
+// switch theme
+btnSwitchTheme.addEventListener('click', () => {    
+    let currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
