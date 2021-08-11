ecfcf59f413e1de12f234d7dc067a7df9c4b2a3d
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-08-11 15:54:19 +0200

    link menu mobile modifications

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 const btnBurger = document.querySelector('#btn-burger');
-const btnCloseBtnBurger = document.querySelector('#btn-close-menu')
+const btnCloseMenu = document.querySelector('#btn-close-menu')
 const asideMenu = document.querySelector('#aside-menu');
+const linkNav = document.getElementsByClassName('link-nav');
 
-// for switch theme
+// switch theme
 const btnSwitchTheme = document.querySelector('#btn-change-mode');
 const body = document.querySelector('#body');
 const containerSite = document.querySelector('.container-site');
 
-
-
-// for the burger menu 
-btnBurger.addEventListener('click', () => {     
-    asideMenu.classList.remove('hide');
-    asideMenu.classList.add('show');  
-    asideMenu.style.width = '70%';
-    btnBurger.classList.add('hide');
-    btnCloseBtnBurger.classList.add('show');    
-});
-
-/*btnCloseBtnBurger.addEventListener('click', () => {   
-    btnCloseBtnBurger.classList.add('hide');
-    asideMenu.classList.remove('show');
-    asideMenu.classList.add('hide');
-});*/
-
-// switch theme
 btnSwitchTheme.addEventListener('click', () => {    
     let currentClass = body.className;
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
 });
+
+
+// for the burger menu 
+btnBurger.addEventListener('click', () => {   
+    asideMenu.classList.add('show');    
+    asideMenu.style.width = '70%';
+    asideMenu.style.transition = '.5s';
+    btnBurger.style.display = "none";
+    btnCloseMenu.style.display = "flex";       
+});
+
+btnCloseMenu.addEventListener('click', () => {     
+    asideMenu.classList.remove('show');
+    asideMenu.style.width = '0%';    
+    asideMenu.style.transition = '.5s';
+    btnBurger.style.display = "flex";
+    btnCloseMenu.style.display = "none";
+});
+
+
+
+for(let i = 0 ; i < linkNav.length ; i++){
+    linkNav[i].addEventListener('click', () => {     
+        asideMenu.classList.remove('show');
+        asideMenu.style.width = '0%';    
+        asideMenu.style.transition = '.5s';
+        btnBurger.style.display = "flex";
+        btnCloseMenu.style.display = "none";
+    });
+};
 
 // typing animation 
 
