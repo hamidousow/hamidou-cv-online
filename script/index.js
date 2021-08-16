253a51c63cd69af8f7530485fbc0dc8fb0a589db
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-08-16 18:34:10 +0200

    updates description section formation

---
+++ script/index.js
@@ -0,0 +1,58 @@
+
+
+function index() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "Styles/index.css") {
+		theme.setAttribute("href", "Styles/index.css");
+	} else {
+		theme.setAttribute("href", "Styles/index.css");
+	}
+}
+function square() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "Styles/square.css") {
+		theme.setAttribute("href", "Styles/square.css");
+	} else {
+		theme.setAttribute("href", "Styles/square.css");
+	}
+}
+function poule() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "Styles/poule.css") {
+		theme.setAttribute("href", "Styles/poule.css");
+	} else {
+		theme.setAttribute("href", "Styles/poule.css");
+	}
+}
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+var theme = document.getElementById("change-style");
+
+// Change the value of href attribute
+// to change the css sheet.
+if (theme.getAttribute("href") != "Styles/square.css") {
+	theme.setAttribute("href", "Styles/square.css");
+} else {
+		theme.setAttribute("href", "Styles/square.css");
+}
+
+
