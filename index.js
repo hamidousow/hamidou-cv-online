5d14a4ff0614ea49cc9c80de6ed984fd0e9773d6
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-11-24 11:48:51 +0100

    new cards created

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const navBg = document.getElementById('nav-mobile');
 const btnSwitchTheme = document.querySelectorAll('.btn-change-mode');
 const btnSwitchThemeMobile = document.getElementById('btn-change-mode-mbl');
 const body = document.querySelector('#body');
-//const containerSite = document.querySelector('.container-site');
 
 for( i = 0 ; i < btnSwitchTheme.length ; i++ ) {
     btnSwitchTheme[i].addEventListener('click', () => {
-        console.log('yes');
         body.classList.toggle('dark-mode');
     });
 }
@@ -35,7 +33,6 @@ btnCloseMenu.addEventListener('click', () => {
 });
 
 // close aside menu if click on link nav mobile devices
-
 for(let j = 0 ; j < linkNav.length ; j++){
     linkNav[j].addEventListener('click', () => {     
         asideMenu.classList.remove('show');
@@ -47,14 +44,90 @@ for(let j = 0 ; j < linkNav.length ; j++){
 };
 
 // bg-color nav when scroll
-
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 80) {
         navBg.classList.add('navBg');
     } else {
         navBg.classList.remove('navBg');
     }
-})
+});
+
+// cards 
+let dataCompetences = [
+    {
+        title: "React js",
+        image: "./images/react-brands.svg"
+        },
+    {
+        title: "Javascript",
+        image: "./images/js-brands.svg"
+    },
+    {
+        title: "Github",
+        image: "./images/github-square-brands.svg"
+    },
+    {
+        title: "Html5",
+        image: "./images/html5-brands.svg"
+    },
+    {
+        title: "Scss",
+        image: "./images/sass-brands.svg"
+    },
+    {
+        title: "Photoshop",
+        image: "./images/photoshop-brand.svg"
+    },
+    {
+        title: "Illustrator",
+        image: "./images/illustrator-brand.svg"
+    }
+]
+
+
+class cardContent {
+    constructor(title, image) {
+        this.title = title
+        this.image = image
+    }
+}
+
+const createNewCard = (comp) => {
+    let newCard = document.createElement("div");
+    const containerCards = document.querySelector('.containerCards');
+    containerCards.append(newCard);
+    newCard.setAttribute('class', "card");
+
+    let cardImg = document.createElement('img');
+    newCard.append(cardImg);
+    cardImg.src = comp.image;
+
+    let newTitle = document.createElement("h2");
+    newCard.append(newTitle);
+    newTitle.innerHTML = comp.title;        
+}
+
+for(let comp of dataCompetences) {
+    createNewCard(comp);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
